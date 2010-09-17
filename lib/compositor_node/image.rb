d665d9f64c8d2c58eef597b5bfd63c0e56dec1a0
Andrew Donaldson <andrew@desto.net>
2010-09-17 21:44:25 +0100

    Update tabs to spaces

--- lib/compositor_node/image.rb
+++ lib/compositor_node/image.rb
@@ -1,13 +1,13 @@
 module CompositorNode
-	class Image
-		attr_accessor :source
+  class Image
+    attr_accessor :source
 
-		def initialize(options = {})
-			@source = options[:source]
-		end
+    def initialize(options = {})
+      @source = options[:source]
+    end
 
-		def execute
-			Magick::ImageList.new(@source)
-		end
-	end
+    def execute
+      Magick::ImageList.new(@source)
+    end
+  end
 end
--- lib/compositor_node/resizer.rb
+++ lib/compositor_node/resizer.rb
@@ -1,15 +1,15 @@
 module CompositorNode
-	class Resizer
-		attr_accessor :source, :width, :height
+  class Resizer
+    attr_accessor :source, :width, :height
 
-		def initialize(options = {})
-			@source = options[:source]
-			@width = options[:width]
-			@height = options[:height]
-		end
-		
-		def execute
-			@source.execute.resize(@width, @height)
-		end
-	end
+    def initialize(options = {})
+      @source = options[:source]
+      @width = options[:width]
+      @height = options[:height]
+    end
+
+    def execute
+      @source.execute.resize(@width, @height)
+    end
+  end
 end
